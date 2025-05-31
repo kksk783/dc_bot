698bed0bd2e434e26bbe2b5a50dc7b31fc88603c
kksk783 <a0981628583@yahoo.com>
2025-05-31 16:40:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+import discord 
+from discord.ext import commands
+
+intents=discord.Intents.default()
+intents.message_content=True
+bot = commands.Bot(command_prefix="",intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f">>Bot ({bot.user}) is online")
+
+
+bot.run("token")
+
+
+
